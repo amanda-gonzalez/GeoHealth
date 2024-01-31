83bf209f8aa43fd1721f3b71408d6a27dce22df1
Joel <joel.harawa@gmail.com>
2024-01-31 13:37:49 -0500

    packages

---
+++ api/db.js
@@ -0,0 +1,8 @@
+const mysql = require('mysql');
+
+const dbConfig = {
+    host: "database-1.ceppaqnusfom.us-east-2.rds.amazonaws.com",
+    user: "admin",
+    password: "GeoHealth2024",
+    database: "database-1"
+}
