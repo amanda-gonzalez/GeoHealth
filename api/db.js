23b8f7f0387b98cc50239c8b79abd97ed29c1f9b
Joel <joel.harawa@gmail.com>
2024-02-05 13:14:11 -0500

    login api

--- api/db.js
+++ api/db.js
@@ -1,8 +1,10 @@
 const mysql = require('mysql');
 
-const dbConfig = {
+const dbConfig = mysql.createConnection( {
     host: "database-geohealth.ceppaqnusfom.us-east-2.rds.amazonaws.com",
     user: "admin",
     password: "GeoHealth2024",
     database: "database-geohealth"
-}
+})
+
+export default dbConfig;
---
+++ app/src/components/Navbar.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+    display: flex;
+    color
+    height: 11
+`;
+const Navbar = () => {
+    return(
+        <Container>
+
+        </Container>
+    )
+}
