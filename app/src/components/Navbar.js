b9ab4feaa68ae6f5d7ee864a854f05f01cade06e
Joel <joel.harawa@gmail.com>
2024-02-05 13:51:22 -0500

    rough front end

--- app/src/components/Navbar.js
+++ app/src/components/Navbar.js
@@ -1,14 +1,24 @@
+import React from "react";
 import styled from "styled-components";
+import {Link} from "react-router-dom";
 
 const Container = styled.div`
     display: flex;
-    color
-    height: 11
+    color: white;
+    height: 10vh;
 `;
+
+const MenuItem = styled(Link)`
+    font-size: 5vh;
+    color: black;
+`;
+
 const Navbar = () => {
     return(
         <Container>
-
+            <MenuItem to="/">GeoHealth</MenuItem>
         </Container>
     )
 }
+
+export default Navbar;
--- app/src/pages/Home.js
+++ app/src/pages/Home.js
@@ -1,11 +1,18 @@
 import React from "react";
+import Navbar from "../components/Navbar";
+import styled from "styled-components";
+const Background = styled.div`
+    background-color: #A4E7F5;
+    display: flex;
+    height: 90vh;
+`;
 
 const Home = () => {
     return (
         <div>
-
+            <Navbar/>
+            <Background/>
         </div>
     )
 }
-
 export default Home;
