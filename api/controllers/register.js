0b01eb49685461422c4bcf3ff24c42651a53438b
Richard Zhong <98974633+Jz8801@users.noreply.github.com>
2024-02-12 13:42:08 -0500

    backend functioning

--- api/controllers/register.js
+++ api/controllers/register.js
@@ -1,7 +1,7 @@
 import dbConfig from "../db.js"
 
 export const registerUser = (req, res) => {
-    const sql = "INSERT INTO Users (username, passwords, email, first_name, last_name) VALUES (?)";
+    const sql = "INSERT INTO Users (username, passwords, email, first_name, last_name) VALUES (?);";
     const values = [
         req.body.username,
         req.body.password,
@@ -11,8 +11,9 @@ export const registerUser = (req, res) => {
     ]
     dbConfig.query(sql, [values], (err, data) => {
         if (err) {
-            return res.json("Error");
+            console.log(err);
+            return res.status(500).json({error: "An error occurred during registration."});
         }
-        return res.json(data);
+        return res.status(201).json({message: "User registered successfully", userData: data});
     })
 }
--- api/db.js
+++ api/db.js
@@ -4,7 +4,7 @@ const dbConfig = mysql.createConnection( {
     host: "database-geohealth.ceppaqnusfom.us-east-2.rds.amazonaws.com",
     user: "admin",
     password: "GeoHealth2024",
-    database: "database-geohealth"
+    database: "GeoHealth"
 })
 
 export default dbConfig;
