34419bc995f2a11096cc4530df7c0cd088533ec0
amanda <amanda.akg.gonzalez@gmail.com>
2024-02-21 13:18:58 -0500

    Update Login.js

--- app/src/pages/Login.js
+++ app/src/pages/Login.js
@@ -2,6 +2,7 @@ import React from "react";
 import Navbar from "../components/Navbar";
 import styled from "styled-components";
 import {Link} from "react-router-dom";
+import './app.css';
 
 const Background = styled.div`
     background-color: #A4E7F5;
@@ -20,17 +21,23 @@ const Form = styled.div`
     box-shadow: 1px 4px 4px rgba(0, 0, 0, 0.25);
 `;
 
-const MenuItem = styled(Link)`
-    font-size: 5vh;
+const Button = styled.button`
+    padding: 10px;
+    padding-top: 30px;
     color: black;
+    text-decoration: none;
 `;
 
-const title = document.getElementById('title');
-title.style.textAlign = 'center';
-
-
-const Input = styled.input`
+const MenuItem = styled(Link)`
+    font-size: smaller;
+    color: black;
+    text-decoration: none;
 `;
+
+
+// const Input = styled.input`
+// `;
+
 const Login = () => {
     return (
         <div>
@@ -40,7 +47,14 @@ const Login = () => {
                     <div id="title">
                         <h1>Login</h1>
                     </div>
-                    <MenuItem to="/register">register</MenuItem>
+                    <div class="credentials">
+                        <h2>Email Address</h2>
+                        <input type="text" id="email" name="email" value="Email" required minlength="12" maxlength="25" size="30"></input>
+                        <h2>Password</h2>
+                        <input type="text" id="password" name="password" value="Password" required minlength="12" maxlength="25" size="30"></input>
+                    </div>
+                    <MenuItem to="/register" id="register">New User? Sign Up Here</MenuItem>
+                    <Button id="login">Login</Button>
                 </Form>
                 
             </Background>
