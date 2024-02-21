5f2d78e87aaa5c9915c316c2dcbb1da8ef23bbd5
amanda <amanda.akg.gonzalez@gmail.com>
2024-02-21 13:19:02 -0500

    Update Register.js

--- app/src/pages/Register.js
+++ app/src/pages/Register.js
@@ -13,19 +13,37 @@ const Background = styled.div`
 
 const Form = styled.div`
     background-color: white;
-    height: 50vh;
+    height: 65vh;
     width: 50vh;
+    border-radius: 10%;
+    box-shadow: 1px 4px 4px rgba(0, 0, 0, 0.25);
 `;
 
-const Input = styled.input`
+const Button = styled.button`
+    padding: 10px;
+    padding-top: 30px;
+    color: black;
+    text-decoration: none;
 `;
+
+// const Input = styled.input`
+// `;
 const Register = () => {
     return (
         <div>
             <Navbar/>
             <Background>
                 <Form>
-                
+                <div id="title">
+                        <h1>Sign Up</h1>
+                    </div>
+                    <div class="credentials">
+                        <h2>Email Address</h2>
+                        <input type="text" id="email" name="email" value="Email" required minlength="12" maxlength="25" size="30"></input>
+                        <h2>Password</h2>
+                        <input type="text" id="password" name="password" value="Password" required minlength="12" maxlength="25" size="30"></input>
+                    </div>
+                    <Button to="/register" id="signup">Sign Up</Button>
                 </Form>
             </Background>
 
