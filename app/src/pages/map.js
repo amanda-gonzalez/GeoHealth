fc5feaa33137730297465af13fbb399ed1aa54c9
Richard Zhong <98974633+Jz8801@users.noreply.github.com>
2024-02-26 20:28:46 -0500

    working google map

--- app/src/pages/map.js
+++ app/src/pages/map.js
@@ -1,7 +1,8 @@
 import React from "react";
 import Navbar from "../components/Navbar";
 import styled from "styled-components";
-import { GoogleMap, useLoadScript, Marker } from '@react-google-maps/api';
+import { useJsApiLoader, GoogleMap, Marker, Autocomplete } from '@react-google-maps/api';
+import { useState } from 'react';
 
 const Background = styled.div`
     background-color: #A4E7F5;
@@ -10,42 +11,44 @@ const Background = styled.div`
 `;
 
 const libraries = ['places'];
+
 const mapContainerStyle = {
-  width: '100vw',
-  height: '100vh',
+  width: '100%',
+  height: '100%',
 };
+
 const center = {
     lat: 40.694340, // default latitude
     lng: -73.986110, // default longitude
   };
 
 const Map = () => {
-    const {isLoaded, loadError} = useLoadScript({
-        googleMapsAPI: 'AIzaSyDjS_SKB2EhV_PEh4nS_v-NkwJsLoerwOs',
-        libraries,
+    const {isLoaded} = useJsApiLoader({
+        googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
+        libraries
     });
 
-    if (loadError) {
-        return <div>Error loading maps</div>;
-    }
+    const [map, setMap] = useState(/** @type google.maps.Map */ (null));
 
     if (!isLoaded) {
-        return <div>Loading maps</div>;
+        return <div>Loading Map</div>;
     }
+
+    console.log(process.env.REACT_APP_GOOGLE_MAPS_API_KEY); // Temporary check
 
     return (
         <div>
             <Navbar/>
-            <Background/>
-            <div>
+            <Background>
             <GoogleMap
                 mapContainerStyle={mapContainerStyle}
                 zoom={10}
                 center={center}
+                onLoad = {map => setMap(map)}
             >
                 <Marker position={center} />
             </GoogleMap>
-            </div>
+            </Background>
         </div>
     )
 }
