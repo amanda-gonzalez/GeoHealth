0e612500d4f4f5563ff789c78b347227c50a7fc4
amanda <amanda.akg.gonzalez@gmail.com>
2024-03-04 13:38:59 -0500

    Create Resources.js

---
+++ app/src/pages/Resources.js
@@ -0,0 +1,7 @@
+import React, {useState} from "react";
+import Navbar from "../components/Navbar";
+import styled from "styled-components";
+import {Link} from "react-router-dom";
+import axios from "axios";
+
+import './app.css';
