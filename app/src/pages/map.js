d0e42633adfeef81438aba7e47b6ce8eb2347ddf
Richard Zhong <98974633+Jz8801@users.noreply.github.com>
2024-03-06 13:17:23 -0500

    added getting direction feature on map

--- app/src/pages/map.js
+++ app/src/pages/map.js
@@ -1,8 +1,8 @@
 import React from "react";
 import Navbar from "../components/Navbar";
 import styled from "styled-components";
-import { useJsApiLoader, GoogleMap, Marker, Autocomplete } from '@react-google-maps/api';
-import { useState } from 'react';
+import { useJsApiLoader, GoogleMap, Marker, Autocomplete, DirectionsRenderer } from '@react-google-maps/api';
+import { useState, useRef } from 'react';
 
 const Background = styled.div`
     background-color: #A4E7F5;
@@ -25,8 +25,17 @@ const Map = () => {
 
     const [map, setMap] = useState(/** @type google.maps.Map */ (null));
     const [userLocation, setUserLocation] = useState(null);
+    const [directionsResponse, setDirectionsResponse] = useState(null);
+    const [distance, setDistance] = useState('');
+    const [duration, setDuration] = useState('');
 
-    const getUserLocation = () => {
+    /** @type React.MutableRefObject<HTMLInputElement> */
+    const sourceRef = useRef();
+    /** @type React.MutableRefObject<HTMLInputElement> */
+    const destinationRef = useRef();
+
+
+    const getUserLocation = async () => {
         if (navigator.geolocation) {
           navigator.geolocation.getCurrentPosition(
             (position) => {
@@ -45,8 +54,29 @@ const Map = () => {
     if (!isLoaded) {
         return <div>Loading Map</div>;
     }
+    
+    async function calculateRoute() {
+        if (sourceRef.current.value === '' || destinationRef.current.value === '') {
+            return;
+        }
+        const directionsService = new window.google.maps.DirectionsService();
+        const result = await directionsService.route({
+            origin: sourceRef.current.value,
+            destination: destinationRef.current.value,
+            travelMode: window.google.maps.TravelMode.DRIVING
+        })
+        setDirectionsResponse(result);
+        setDistance(result.routes[0].legs[0].distance.text);
+        setDuration(result.routes[0].legs[0].duration.text);
+    }
 
-    console.log(userLocation);
+    function clearRoute() {
+        setDirectionsResponse(null);
+        setDistance('')
+        setDuration('')
+        sourceRef.current.value = '';
+        destinationRef.current.value = '';
+    }
 
     return (
         <div>
@@ -54,7 +84,7 @@ const Map = () => {
             <Background>
                 <GoogleMap
                     mapContainerStyle={mapContainerStyle}
-                    zoom={10}
+                    zoom={15}
                     center={userLocation}
                     options = {{
                         streetViewControl: false,
@@ -63,7 +93,29 @@ const Map = () => {
                     onLoad = {getUserLocation}
                 >
                     <Marker position={userLocation} />
+                    {directionsResponse && <DirectionsRenderer directions={directionsResponse}/>}
                 </GoogleMap>
+
+                <div>
+                    <Autocomplete>
+                        <input
+                            type="text"
+                            placeholder="Source"
+                            ref={sourceRef}
+                        />
+                    </Autocomplete>
+                    <Autocomplete>
+                    <input
+                        type="text"
+                        placeholder="Destination"
+                        ref={destinationRef}
+                    />
+                    </Autocomplete>
+                    <button type='submit' onClick={calculateRoute}>
+                        Calculate Route
+                    </button>
+                    <button aria-label='center back' onClick={clearRoute}></button> 
+                </div>
             </Background>
         </div>
     )
