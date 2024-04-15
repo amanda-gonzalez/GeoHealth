b9097546cc35f86e823cc98ffb6b80b34bd21046
delizee <70963596+delizee@users.noreply.github.com>
2024-04-15 04:15:43 -0400

    Update Resources

--- app/src/pages/Resources.js
+++ app/src/pages/Resources.js
@@ -3,6 +3,10 @@ import Navbar from "../components/Navbar";
 import styled from "styled-components";
 import {Link} from "react-router-dom";
 import axios from "axios";
+import img1 from "../images/rsrc-img1.png";
+import img2 from "../images/rsrc-img2.png";
+import img3 from "../images/rsrc-img3.png";
+import img4 from "../images/rsrc-img4.png";
 
 import './app.css';
 
@@ -10,21 +14,61 @@ const Resources = () =>{
     return(
     <div id="resources">
         <Navbar/>
-        <div id="mainArea">
-            <div id="artBox">
-                <div id="contentBox">
-                    <div id="content">
-                        <h3>Article Topic blah blah blah blah</h3>
-                        <p>Article Title... </p>
+        <div id="rmainArea">
+            <div class="artBox">
+                <div id="resourceMedia">
+                    <div class="rImg">
+                        <img src={img1}></img>
+                    </div>
+                    <div id="contentBox">
+                        <div id="content">
+                            <h3>Article Topic: Covid 19</h3>
+                            <p>Article Title: What You Need to Know About Covid 19 Now</p>
+                            <p>Article Author: NYC Department of Health</p>
+                        </div>
                     </div>
                 </div>
             </div>
-            <div id="resourceMedia">
-                <div class="rArticle">
-                    <img></img>
+            <div class="artBox">
+                <div id="resourceMedia">
+                    <div class="rImg">
+                        <img src={img2}></img>
+                    </div>
+                    <div id="contentBox">
+                        <div id="content">
+                            <h3>Article Topic: Nutrition</h3>
+                            <p>Article Title: Are plant-based meat substitutes really better for the heart than meat options?</p>
+                            <p>Article Author: Paul Ian Cross Ph.D.</p>
+                        </div>
+                    </div>
                 </div>
-                <div class="rArticle">
-                    <img></img>
+            </div>
+            <div class="artBox">
+                <div id="resourceMedia">
+                    <div class="rImg">
+                        <img src={img3}></img>
+                    </div>
+                    <div id="contentBox">
+                        <div id="content">
+                            <h3>Article Topic: Gut Health</h3>
+                            <p>Article Title: In Conversation: What do we know about the gut microbiome in IBD?</p>
+                            <p>Article Author:  Maria Cohut Ph.D.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="artBox">
+                <div id="resourceMedia">
+                    <div class="rImg">
+                        <img src={img4}></img>
+                    </div>
+                    <div id="contentBox">
+                        <div id="content">
+                            <h3>Article Topic: Health Insurance</h3>
+                            <p>Article Title: Health Insurance: Enrollment Counselors</p>
+                            <p>Article Author: NYC Department of Health</p>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
