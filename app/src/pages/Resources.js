9a52fde20a02f7fd61f338875656e723b6743a5f
amanda <amanda.akg.gonzalez@gmail.com>
2024-04-22 23:26:22 -0400

    Update Resources.js

--- app/src/pages/Resources.js
+++ app/src/pages/Resources.js
@@ -62,7 +62,22 @@ function displayNews(articles){
     }
 }
 
-//window.addEventListener('load', getNews);
+window.addEventListener('load', saveFunction);
+
+function saveFunction(){
+    let stars = document.querySelectorAll('.bi-star');
+    for(let i = 0; i < stars.length; i++){
+        stars[i].addEventListener('click', () => {
+            if(stars[i].classList.contains('fillstar')){
+                stars[i].classList.remove('fillstar')
+                stars[i].innerHTML = '<path d="M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.522-3.356c.33-.314.16-.888-.282-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767-3.686 1.894.694-3.957a.56.56 0 0 0-.163-.505L1.71 6.745l4.052-.576a.53.53 0 0 0 .393-.288L8 2.223l1.847 3.658a.53.53 0 0 0 .393.288l4.052.575-2.906 2.77a.56.56 0 0 0-.163.506l.694 3.957-3.686-1.894a.5.5 0 0 0-.461 0z"/>';
+            }else{
+                stars[i].classList.add('fillstar');
+                stars[i].innerHTML = '<path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>';
+            }
+        });    
+    }
+}
 
 const Resources = () =>{
     getNews();
@@ -73,13 +88,16 @@ const Resources = () =>{
             <div class="artBox">
                 <div id="resourceMedia">
                     <div class="rImg">
-                        <img class="rInnerImg" src={img1}></img>
+                        <img class="rInnerImg"></img><br/>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" fill="currentColor" class="bi bi-star" viewBox="0 0 16 16">
+                            <path d="M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.522-3.356c.33-.314.16-.888-.282-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767-3.686 1.894.694-3.957a.56.56 0 0 0-.163-.505L1.71 6.745l4.052-.576a.53.53 0 0 0 .393-.288L8 2.223l1.847 3.658a.53.53 0 0 0 .393.288l4.052.575-2.906 2.77a.56.56 0 0 0-.163.506l.694 3.957-3.686-1.894a.5.5 0 0 0-.461 0z"/>
+                        </svg>
                     </div>
                     <div id="contentBox">
                         <div id="content">
-                            <h3 class="rSource">Article Topic: COVID-19</h3>
-                            <a class="rLink" href="https://www.nyc.gov/site/doh/covid/covid-19-main.page" target="_blank">Article Title: What You Need to Know About Covid 19 Now</a>
-                            <p class="rAuthor">Article Author: NYC Department of Health</p>
+                            <h3 class="rSource"></h3>
+                            <a class="rLink" href="https://www.nyc.gov/site/doh/covid/covid-19-main.page" target="_blank"></a>
+                            <p class="rAuthor"></p>
                         </div>
                     </div>
                 </div>
@@ -87,13 +105,17 @@ const Resources = () =>{
             <div class="artBox">
                 <div id="resourceMedia">
                     <div class="rImg">
-                        <img class="rInnerImg" src={img2}></img>
+                        <img class="rInnerImg"></img>
+                        <br/>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" fill="currentColor" class="bi bi-star" viewBox="0 0 16 16">
+                            <path d="M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.522-3.356c.33-.314.16-.888-.282-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767-3.686 1.894.694-3.957a.56.56 0 0 0-.163-.505L1.71 6.745l4.052-.576a.53.53 0 0 0 .393-.288L8 2.223l1.847 3.658a.53.53 0 0 0 .393.288l4.052.575-2.906 2.77a.56.56 0 0 0-.163.506l.694 3.957-3.686-1.894a.5.5 0 0 0-.461 0z"/>
+                        </svg>
                     </div>
                     <div id="contentBox">
                         <div id="content">
-                            <h3 class="rSource">Article Topic: Nutrition</h3>
-                            <a class="rLink" href="https://www.medicalnewstoday.com/articles/are-plant-based-meat-substitutes-really-better-for-the-heart-than-meat-options" target="_blank">Article Title: Are plant-based meat substitutes really better for the heart than meat options?</a>
-                            <p class="rAuthor">Article Author: Paul Ian Cross Ph.D.</p>
+                            <h3 class="rSource"></h3>
+                            <a class="rLink" href="https://www.medicalnewstoday.com/articles/are-plant-based-meat-substitutes-really-better-for-the-heart-than-meat-options" target="_blank"></a>
+                            <p class="rAuthor"></p>
                         </div>
                     </div>
                 </div>
@@ -101,13 +123,16 @@ const Resources = () =>{
             <div class="artBox">
                 <div id="resourceMedia">
                     <div class="rImg">
-                        <img class="rInnerImg" src={img3}></img>
+                        <img class="rInnerImg"></img><br/>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" fill="currentColor" class="bi bi-star" viewBox="0 0 16 16">
+                            <path d="M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.522-3.356c.33-.314.16-.888-.282-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767-3.686 1.894.694-3.957a.56.56 0 0 0-.163-.505L1.71 6.745l4.052-.576a.53.53 0 0 0 .393-.288L8 2.223l1.847 3.658a.53.53 0 0 0 .393.288l4.052.575-2.906 2.77a.56.56 0 0 0-.163.506l.694 3.957-3.686-1.894a.5.5 0 0 0-.461 0z"/>
+                        </svg>
                     </div>
                     <div id="contentBox">
                         <div id="content">
-                            <h3 class="rSource">Article Topic: Gut Health</h3>
-                            <a class="rLink" href="https://www.medicalnewstoday.com/articles/in-conversation-what-do-we-know-about-the-gut-microbiome-in-ibd" target="_blank">Article Title: In Conversation: What do we know about the gut microbiome in IBD?</a>
-                            <p class="rAuthor">Article Author:  Maria Cohut Ph.D.</p>
+                            <h3 class="rSource"></h3>
+                            <a class="rLink" href="https://www.medicalnewstoday.com/articles/in-conversation-what-do-we-know-about-the-gut-microbiome-in-ibd" target="_blank"></a>
+                            <p class="rAuthor"></p>
                         </div>
                     </div>
                 </div>
@@ -115,13 +140,16 @@ const Resources = () =>{
             <div class="artBox">
                 <div id="resourceMedia">
                     <div class="rImg">
-                        <img class="rInnerImg" src={img4}></img>
+                        <img class="rInnerImg"></img><br/>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" fill="currentColor" class="bi bi-star" viewBox="0 0 16 16">
+                            <path d="M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.522-3.356c.33-.314.16-.888-.282-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767-3.686 1.894.694-3.957a.56.56 0 0 0-.163-.505L1.71 6.745l4.052-.576a.53.53 0 0 0 .393-.288L8 2.223l1.847 3.658a.53.53 0 0 0 .393.288l4.052.575-2.906 2.77a.56.56 0 0 0-.163.506l.694 3.957-3.686-1.894a.5.5 0 0 0-.461 0z"/>
+                        </svg>
                     </div>
                     <div id="contentBox">
                         <div id="content">
-                            <h3 class="rSource">Article Topic: Health Insurance</h3>
-                            <a class="rLink" href="https://www.nyc.gov/site/doh/health/health-topics/health-insurance.page" target="_blank">Article Title: Health Insurance: Enrollment Counselors</a>
-                            <p class="rAuthor">Article Author: NYC Department of Health</p>
+                            <h3 class="rSource"></h3>
+                            <a class="rLink" href="https://www.nyc.gov/site/doh/health/health-topics/health-insurance.page" target="_blank"></a>
+                            <p class="rAuthor"></p>
                         </div>
                     </div>
                 </div>
