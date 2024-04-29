c4b8ba0df4377f5b284641a398fbca33d339d245
amanda <amanda.akg.gonzalez@gmail.com>
2024-04-28 21:43:28 -0400

    Update App.test.js

--- app/src/App.test.js
+++ app/src/App.test.js
@@ -1,8 +1,14 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen , cleanup} from '@testing-library/react';
 import App from './App';
+import Resources from "./pages/Resources";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+// test('renders learn react link', () => {
+//   render(<App />);
+//   const linkElement = screen.getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
+
+test('renders resources page', () => {
+  render(<Resources />);
+  expect(screen.getByRole("svg")).toBeInTheDocument();
 });
